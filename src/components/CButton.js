4431f2a3893c3a97e9997a2f49b4a5cbcb0713d4
khenya <abrendakhenya@gmail.com>
2024-07-05 22:56:48 -0400

    fix: styles

--- src/components/CButton.js
+++ src/components/CButton.js
@@ -3,17 +3,18 @@ import React from 'react';
 import { Pressable, Text } from 'react-native';
 import { styles } from '../config/theme/app-theme';
 
-export const CButton = ({ textButton, bgColor, action }) => {
+export const CButton = ({ textButton, bgColor, textColor, action, customStyle }) => {
   return (
     <Pressable
       onPress={action}
       style={({ pressed }) => ({
         ...styles.button,
+        ...customStyle,
         backgroundColor: bgColor,
         opacity: pressed ? 0.5 : 1,
       })}
     >
-      <Text style={styles.buttonText}>{textButton}</Text>
+      <Text style={[styles.buttonText, { color: textColor }]}>{textButton}</Text>
     </Pressable>
   );
 };
